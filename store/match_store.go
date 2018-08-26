2455da8085eb67433a75c401f540158d5d52766f
albshin <albshin07@gmail.com>
2018-08-26 13:28:53 -0400

    Move player store functions into the correct file

--- store/match_store.go
+++ store/match_store.go
@@ -168,19 +168,3 @@ func (db *PGStore) GetMatchesByPlayerID(playerID int, matchFilters *model.MatchF
 	}
 	return matches, nil
 }
-
-func (db *PGStore) GetPlayer(playerID int) (*model.Player, error) {
-	var player model.Player
-	if err := db.Get(&player, "SELECT * FROM players WHERE id=$1", playerID); err != nil {
-		return nil, err
-	}
-	return &player, nil
-}
-
-func (db *PGStore) GetPlayers(pageParams *model.Pagination) ([]*model.Player, error) {
-	players := make([]*model.Player, 0)
-	if err := db.Select(&players, "SELECT * FROM players LIMIT $1 OFFSET $2", pageParams.Limit, pageParams.Offset()); err != nil {
-		return nil, err
-	}
-	return players, nil
-}
