6f13afd5f41d6bd0a31a267311eb13bb8917083f
albshin <albshin07@gmail.com>
2018-08-31 18:28:49 -0400

    Change variable name

--- store/report_store.go
+++ store/report_store.go
@@ -2,19 +2,19 @@ package store
 
 import "github.com/albshin/tekkenmatchdb-backend/model"
 
-func (db *PGStore) CreateMatchReport(req *model.MatchReport) (*model.MatchReport, error) {
+func (db *PGStore) CreateMatchReport(mr *model.MatchReport) (*model.MatchReport, error) {
 	q := `
 	INSERT INTO match_reports
 	(match_id, data)
 	VALUES (:match_id, :data)
 	`
-	rows, err := db.NamedQuery(q, req)
+	rows, err := db.NamedQuery(q, mr)
 	if err != nil {
 		return nil, err
 	}
 	if rows.Next() {
-		rows.Scan(&req.ID)
+		rows.Scan(&mr.ID)
 		rows.Close()
 	}
-	return req, nil
+	return mr, nil
 }
